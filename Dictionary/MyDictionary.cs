628c71d28bcae3b751c7f36bf818331ccfd71f69
gulcearda <77541794+gulcearda@users.noreply.github.com>
2021-01-23 18:59:05 +0300

    odev

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Dictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        TKey[] anahtarlar;
+        TValue[] degerler;
+
+        public MyDictionary()
+        {
+            anahtarlar = new TKey[0];
+            degerler = new TValue[0];
+
+        }
+
+
+        public void Add(TKey anahtar, TValue deger)
+        {
+
+            TKey[] tempAnahtar = anahtarlar;
+            
+            anahtarlar = new TKey[anahtarlar.Length + 1];
+
+            for (int i = 0; i < tempAnahtar.Length; i++)
+            {
+                anahtarlar[i] = tempAnahtar[i];
+            }
+
+            anahtarlar[anahtarlar.Length - 1] = anahtar;
+
+            TValue[] tempDegerler = degerler;
+
+
+            degerler = new TValue[degerler.Length + 1];
+
+            for (int i = 0; i < tempDegerler.Length; i++)
+            {
+                degerler[i] = tempDegerler[i];
+            }
+
+            degerler[degerler.Length - 1] = deger;
+
+
+
+        }
+
+       
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> vatandaslar = new MyDictionary<int, string>();
+            vatandaslar.Add(123456789, "Gülce Arda");
+            vatandaslar.Add(234567891, "Ayşe Arda");
+
+        }
+    }
+}
